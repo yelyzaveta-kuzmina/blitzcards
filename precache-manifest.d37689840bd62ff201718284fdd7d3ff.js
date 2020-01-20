3ba293110bf6e1088a1f779888202c8aba1f0c5b
Yelyzaveta <elizaveta.kzm@gmail.com>
2020-01-20 13:13:31 +0200

    Updates

---
+++ precache-manifest.d37689840bd62ff201718284fdd7d3ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b850fe5b336ec31644ca1ad8124247",
+    "url": "/blitzcards/index.html"
+  },
+  {
+    "revision": "c511f2b507a0a41a43df",
+    "url": "/blitzcards/static/css/main.58c97d8e.chunk.css"
+  },
+  {
+    "revision": "0560950f3245d83401ed",
+    "url": "/blitzcards/static/js/2.11af10a9.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/blitzcards/static/js/2.11af10a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c511f2b507a0a41a43df",
+    "url": "/blitzcards/static/js/main.43d9f1b4.chunk.js"
+  },
+  {
+    "revision": "abc2b4b14672f5e82f1e",
+    "url": "/blitzcards/static/js/runtime-main.84cf8f9d.js"
+  }
+]);
