fc30add5402dbe7b571454192b3af1a7eed6a372
Yelyzaveta <elizaveta.kzm@gmail.com>
2020-10-06 00:02:33 +0200

    game setting: keyboard side menu navigation

--- src/modules/game-settings-window/index.js
+++ src/modules/game-settings-window/index.js
@@ -1,22 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import ToPreviousPageButton from '../../components/to-previous-page-button';
 import SideMenu from './side-menu';
 import TranslationDirectionSetting from './translation-direction';
 import styles from './styles.module.scss';
 
+const items = [
+  {
+    label: 'Translation Direction'
+  },
+  {
+    label: 'Cursor'
+  }
+];
+
+const KeyboardKey = {
+  ARROW_UP: 'ArrowUp',
+  ARROW_DOWN: 'ArrowDown'
+};
+
+// const ALLOWED_KEYS = ['ArrowUp', 'ArrowDown'];
+
 const GameSettingsWindow = () => {
   const { sourceLanguage, targetLanguage, category: categoryName } = useRouteMatch().params;
-  const [currentSetting, setCurrentSetting] = useState('Translation Direction');
+  const [selectedSettingIndex, setSelectedSettingIndex] = useState(0);
+  const currentSetting = items[selectedSettingIndex].label;
 
-  const items = [
-    {
-      label: 'Translation Direction'
-    },
-    {
-      label: 'Cursor'
-    }
-  ];
+  useEffect(() => {
+    const openPreviousSettingPage = () =>
+      setSelectedSettingIndex((index) => (index - 1 + items.length) % items.length);
+    const openNextSettingPage = () =>
+      setSelectedSettingIndex((index) => (index + 1) % items.length);
+
+    const handleKeyDown = (event) => {
+      if (event.key === KeyboardKey.ARROW_UP) {
+        openPreviousSettingPage();
+      }
+      if (event.key === KeyboardKey.ARROW_DOWN) {
+        openNextSettingPage();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <div className={styles.settingsWrapper}>
@@ -30,7 +59,7 @@ const GameSettingsWindow = () => {
             item={item}
             selectedItem={currentSetting}
             key={index}
-            onClick={() => setCurrentSetting(item.label)}
+            onClick={() => setSelectedSettingIndex(index)}
           />
         ))}
       </div>
